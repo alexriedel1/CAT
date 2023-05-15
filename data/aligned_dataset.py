9dc90843ff7a7a229b19d8932acf0bce7ebb0912
Alexander Riedel <54716527+alexriedel1@users.noreply.github.com>
2023-05-15 10:20:02 +0200

    torch in dataset

--- data/aligned_dataset.py
+++ data/aligned_dataset.py
@@ -5,6 +5,7 @@ from PIL import Image
 from data.base_dataset import BaseDataset, get_params, get_transform
 from data.image_folder import make_dataset
 import torchvision.transforms as transforms
+import torch
 
 
 class AlignedDataset(BaseDataset):
