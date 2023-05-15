07521a13234bea2b39ea6af59704c4e91e90f7f9
Alexander Riedel <54716527+alexriedel1@users.noreply.github.com>
2023-05-15 12:47:36 +0200

    standard configuration

--- scripts/pix2pix/unfilter/train_inception_teacher.sh
+++ scripts/pix2pix/unfilter/train_inception_teacher.sh
@@ -19,5 +19,5 @@ python train.py --dataroot /content/filtered_notfiltered_match_align \
   --save_epoch_freq 1 --save_latest_freq 2000 \
   --eval_batch_size 8 \
   --direction AtoB \
-  --b2b_prob 0.3 \
-  --preprocess random_rsz \
+  --b2b_prob 0.0 \
+  --preprocess resize_and_crop \ 
